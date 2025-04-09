25db3838236828d6762c7ec4a697274175ff897f
EnchantedDonutStudioz <EnchantedDonutStudioz@gmail.com>
2025-04-08 21:34:47 -0400

    lock in rogo

--- app.js
+++ app.js
@@ -25,8 +25,8 @@ app.use((req, res) => {
 server.on("request", (req, res) => {
     res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
     res.setHeader("Cross-Origin-Embedder-Policy", "anonymous");
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     app(req, res);
 });
 
