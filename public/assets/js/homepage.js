391bacadb890cae69a9b76bc64daaf0a3c0e945c
EnchantedDonutStudioz <EnchantedDonutStudioz@gmail.com>
2025-06-24 14:56:43 -0400

    Spark Redesign Part 2; More games

--- public/assets/js/homepage.js
+++ public/assets/js/homepage.js
@@ -1,4 +1,5 @@
-// Fetch and display games
+const allGamesButton = document.getElementById('all-games');
+
 fetch('/assets/json/topgames.json')
     .then(response => response.json())
     .then(games => {
@@ -25,3 +26,34 @@ fetch('/assets/json/topgames.json')
     .catch(error => {
         console.error('Error loading games:', error);
     });
+
+fetch('/assets/json/hotgames.json')
+    .then(response => response.json())
+    .then(games => {
+        const topGamesGrid = document.getElementById('hot-games-grid');
+
+        games.forEach(game => {
+            const gameCard = document.createElement('div');
+            gameCard.className = 'game-card';
+            gameCard.style.backgroundImage = `url(${game.image})`;
+
+            const gameLink = document.createElement('a');
+            gameLink.href = `/play/${game.name}`;
+            gameLink.className = 'game-link';
+
+            const gameName = document.createElement('div');
+            gameName.className = 'game-name';
+            gameName.textContent = game.name;
+
+            gameLink.appendChild(gameName);
+            gameCard.appendChild(gameLink);
+            topGamesGrid.appendChild(gameCard);
+        });
+    })
+    .catch(error => {
+        console.error('Error loading games:', error);
+    });
+
+allGamesButton.addEventListener('click', () => {
+    window.location.href = '/games';
+});
